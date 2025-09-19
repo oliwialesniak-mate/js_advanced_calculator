5110a761ce5d9651593335928b27ccf65496eabb
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-19 22:20:56 +0200

    Fix: define makeCalculator and remove console.log

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,18 +1,15 @@
-/**
- * @returns {object}
- */
 function makeCalculator() {
   return {
     result: 0,
-    add(value) { return this.result + value; },
-    subtract(value) { return this.result - value; },
-    multiply(value) { return this.result * value; },
-    divide(value) { return this.result / value; },
-    operate(callback, value) {
-      this.result = callback.call(this, value);
-      return this;
-    },
+    add(value) { this.result += value; return this; },
+    subtract(value) { this.result -= value; return this; },
+    multiply(value) { this.result *= value; return this; },
+    divide(value) { this.result /= value; return this; },
     reset() { this.result = 0; return this; },
+    operate(callback, number) {
+      if (typeof callback === 'function') callback.call(this, number);
+      return this;
+    }
   };
 }
 
